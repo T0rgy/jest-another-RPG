3b023e93a9a5c2dff41a10c030b6b2d92948fb15
Torgy <jctorge@gmail.com>
2022-04-26 18:25:43 -0500

    test __mock__

---
+++ __test__/Player.test.js
@@ -0,0 +1,23 @@
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+
+// console.log(new Potion());
+
+
+const { exportAllDeclaration } = require("@babel/types");
+const { default: TestRunner } = require("jest-runner");
+const Player = require("../lib/Player");
+const { JestHook } = require('jest-watcher');
+
+test('creates a player object', () => {
+    const player = new Player('Dave');
+
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
+});
